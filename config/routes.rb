ddf6eede0db12c98d0a56e4455d8fc8369a6c864
Richard Casanova <ricardofer2412@hotmail.com>
2019-01-10 15:10:53 -0500

    add home

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   resources :buy_backs
+  root 'buy_backs#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
